c0a49a9a4a12fb87967abc2e2612d36f82a5d768
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-01-25 15:41:04 +0000

    Conditionals

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,59 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+this_is_true = true
+puts this_is_true
 
 # Boolean Expressions
+puts 3 == 2 # equality - double equals sign
+puts 3 != 2 # non-equality
+puts 3 > 2
+puts 3 < 2
 
 # If Conditional Logic
+if 3 == 2
+    puts "this should never run"
+end
+
+if 3 > 2
+    puts "this should never run"
+    puts "yeah, math"
+
 
 # If/Else Conditional Logic
+if 3 == 2
+    puts "this should never be displayed"
+Else
+    puts "this should always be displayed"
+end
+
+user_entered_password = "tacos"
+real_password = "secret"
+
+if user_entered_password == real_password
+    puts "you're in!"
+Else
+    puts "wrong password"
+end
+
+
 
 # Elsif Conditional Logic
+your_team_score = 2
+other_team_score = 2
+if your_team_score > other_team_score
+    puts "you won!"
+elsif your_team_score == other_team_score
+    puts "it was a tie"
+else
+    puts "sorry, you lost :("
+end
+
 
 # Combining Expressions
+temp = 68
+precip = 0
+if temp >= 65 && temp <= 90 && precip == 0
+    puts "it's perfect outside!"
+end
+end
