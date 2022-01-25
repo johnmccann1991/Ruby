96305700ddd3f2481521df8beab1ecb77d305888
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-01-25 16:34:46 +0000

    arrays lab

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,37 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_foods = ["tacos", "cookies", "nachos", "ice cream"]
+puts favorite_foods
+
 
 # Accessing the array
+we_have_to_go_back = [4, 8, 15, 16, 23, 42]
+puts we_have_to_go_back
+
+mixed_array = ["tacos", 12, true]
+
+shopping_list = [["kale", "raisings", "sticks"], ["beer", "tacos"]]
+puts shopping_list.inspect
+
+puts favorite_foods[0]
+puts favorite_foods[1]
+puts favorite_foods[2]
+puts favorite_foods[3]
+puts favorite_foods[-1]
+
+puts shopping_list [1].inspect
+puts shopping_list [1][1]
+
 
 # Add to the array
+favorite_foods.push ("more tacos")
+favorite_foods = favorite_foods + ["fries", "ramen"]
+puts favorite_foods.inspect
+
+
+
+
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -14,3 +14,16 @@
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html
+
+my_list = ["milk", "eggs", "bacon"]
+friend_list = ["beer", "cookies", "apples"]
+
+combined_list = my_list + friend_list
+
+puts combined_list.inspect
+
+
+
+
+
+
